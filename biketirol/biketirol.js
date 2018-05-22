71d3b194a04ab1107da451a24d683d875f041c7b
Marianne Abriel <marianne.abriel@gmx.net>
2018-05-22 17:45:50 +0200

    biketirol Tourdaten

--- biketirol/biketirol.js
+++ biketirol/biketirol.js
@@ -1,33 +1,7 @@
-/*
-    Vorbereitung: GPX Track herunterladen und nach GeoJSON konvertieren
-    -------------------------------------------------------------------
-    Datenquelle https://www.data.gv.at/suche/?search-term=bike+trail+tirol&searchIn=catalog
-    Download Einzeletappen / Zur Ressource ...
-    Alle Dateien im unterverzeichnis data/ ablegen
-    Die .gpx Datei der eigenen Etappe als etappe00.gpx speichern
-    Die .gpx Datei über https://mapbox.github.io/togeojson/ in .geojson umwandeln und als etappe00.geojson speichern
-    Die etappe00.geojson Datei in ein Javascript Objekt umwandeln und als etappe00.geojson.js speichern
 
-    -> statt 00 natürlich die eigene Etappe (z.B. 01,02, ...25)
-*/
-
-// eine neue Leaflet Karte definieren
-
-// Grundkartenlayer mit OSM, basemap.at, Elektronische Karte Tirol (Sommer, Winter, Orthophoto jeweils mit Beschriftung) über L.featureGroup([]) definieren
-// WMTS URLs siehe https://www.data.gv.at/katalog/dataset/land-tirol_elektronischekartetirol
-
-// Maßstab metrisch ohne inch
-
-// Start- und Endpunkte der Route als Marker mit Popup, Namen, Wikipedia Link und passenden Icons für Start/Ziel von https://mapicons.mapsmarker.com/
-
-// GeoJSON Track als Linie in der Karte einzeichnen und auf Ausschnitt zoomen
-// Einbauen nicht über async, sondern über ein L.geoJSON() mit einem Javascript Objekt (wie beim ersten Stadtspaziergang Wien Beispiel)
-
-// Baselayer control für OSM, basemap.at, Elektronische Karte Tirol hinzufügen
-
-// Overlay controls zum unabhängigem Ein-/Ausschalten der Route und Marker hinzufügen
-
-let myMap = L.map("map"); 
+let myMap = L.map("map", {
+	fullscreenControl: true
+	}); 
 let markerGroup = L.featureGroup();
 let myLayers = {
     osm : L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"), 
@@ -111,12 +85,40 @@ let zielMarker = L.marker (ziel, {
 	}).addTo(markerGroup);
 zielMarker.bindPopup("<p> Ziel in Tannheim</p><img style='width:100px' 7> <a href='https://de.wikipedia.org/wiki/Tannheimer_Tal' > Mehr Information zum Tannheimer Tal</a> ");
 
-let geojson = L.geoJSON(etappendata).addTo(markerGroup);
-geojson.bindPopup(function(Layer){
-	console.log("Layer for Popup:", layer.feature.geometry);
-	const props = layer.feature.geometry;
-	const popupText = `<p>${props.coordinates}</p>`;
-	return popupText;
-});
+//let geojson = L.geoJSON(etappendata).addTo(markerGroup);
+//geojson.bindPopup(function(Layer){
+//	console.log("Layer for Popup:", layer.feature.geometry);
+//	const props = layer.feature.geometry;
+//	const popupText = `<p>${props.coordinates}</p>`;
+//	return popupText;
+//});
 
-myMap.fitBounds(markerGroup.getBounds());
+let gpxTrack = new L.GPX("data/etappe02.gpx", {
+	async : true
+}).addTo(markerGroup);
+gpxTrack.on("loaded", function(evt) {
+	console.log("get_distance",evt.target.get_distance().toFixed(0))
+	console.log("get_elevation_min",evt.target.get_elevation_min().toFixed(0))
+	console.log("get_elevation_max",evt.target.get_elevation_max().toFixed(0))
+	console.log("get_elevation_gain",evt.target.get_elevation_gain().toFixed(0))
+	console.log("get_elevation_loss",evt.target.get_elevation_loss().toFixed(0))
+	
+	let laenge = evt.target.get_distance().toFixed(0);
+	document.getElementById("laenge").innerHTML = laenge;
+
+	let tiefsterPunkt = evt.target.get_elevation_min().toFixed(0);
+	document.getElementById("tiefster Punkt").innerHTML = tiefsterPunkt;
+
+	let hoechsterPunkt = evt.target.get_elevation_max().toFixed(0);
+	document.getElementById("höchster Punkt").innerHTML = hoechsterPunkt;
+
+	let aufstieg = evt.target.get_elevation_gain().toFixed(0);
+	document.getElementById("Aufstieg").innerHTML = aufstieg;
+
+	let abstieg = evt.target.get_elevation_loss().toFixed(0);
+	document.getElementById("Abstieg").innerHTML = abstieg;
+
+	myMap.fitBounds(evt.target.getBounds());
+})
+
+//myMap.fitBounds(markerGroup.getBounds());
